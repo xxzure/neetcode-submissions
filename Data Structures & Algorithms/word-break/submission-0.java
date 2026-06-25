e149806e9bf481df1e41148f0c1a4ab72b43e610
Jun Xiao <xxzure1996@gmail.com>
2026-06-24 21:19:44 -0400

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (String word : wordDict) {
+                int len = word.length();
+                if (i >= len && dp[i - len]) {
+                    if (s.substring(i - len, i).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
